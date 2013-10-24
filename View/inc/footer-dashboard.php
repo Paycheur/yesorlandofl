fcfe6efcc97e33516ad8f6cc3e98b8ba1c837d68
Sherif SALEH <sherifsaleh111@gmail.com>
2013-10-24 23:04:10 +0200

    First lock to the dashboard, coming up with the minmized version

--- View/inc/footer-dashboard.php
+++ View/inc/footer-dashboard.php
@@ -1,129 +1,43 @@
-</div>
 
-<!-- FOOTER -->
-<footer class="bg-white">
-	<div class="container">
-		<section class="row padding-2em">
-			<div class="col-lg-6">
-				<hgroup >
-				<h2 class="margin-zero lh-80 "><a class=" serif bold size-16" rel="tab"  href="index.php">THE <span class="italic">PORTFOLIO</span> GROUP</a></h2>
-				<h6 class="margin-zero lh-80">
-				<a class="color-sec  sans  size-14" rel="tab"  href="index.php">International real Estate Experts </a>
-				</h6>
-				</hgroup>
-				<p><i class="icon-phone"></i> 407-697-2176  | <i class="icon-mobile-phone"></i> 407-697-2176</p>
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="/Assets/dashboard/js/jquery.js"></script>
+    <script src="/Assets/dashboard/js/jquery-1.8.3.min.js"></script>
+    <script src="/Assets/dashboard/js/bootstrap.min.js"></script>
+    <script src="/Assets/dashboard/js/jquery.scrollTo.min.js"></script>
+    <script src="/Assets/dashboard/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="/Assets/dashboard/js/jquery.sparkline.js" type="text/javascript"></script>
+    <script src="/Assets/dashboard/assets/jquery-easy-pie-chart/jquery.easy-pie-chart.js"></script>
+    <script src="/Assets/dashboard/js/owl.carousel.js" ></script>
+    <script src="/Assets/dashboard/js/jquery.customSelect.min.js" ></script>
 
-			</div>
-			<div class="col-lg-6">
-				<ul class="list-inline padding-1em">
-					<li><a href="#" class="btn btn-default"><i class="icon-facebook"></i></a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-twitter"></i> </a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-linkedin"></i> </a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-foursquare"></i></a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-pinterest"></i></a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-google-plus"></i></a></li>
-					<li><a href="#" class="btn btn-default"><i class="icon-skype"></i></a></li>
-				</ul>
-			</div>
-		</section>
-		<div class="row">
-			<aside class="col-lg-3">
-				<h4>KEY WORDS </h4>
-				<ul class="list-no-bullets">
-					<li><a href="#">buy lorem bla bla Orlando</a></li>
-					<li><a href="#">Rent lorem bla bla Orlando</a></li>
-					<li><a href="#">Rent lorem bla bla Orlando</a></li>
-					<li><a href="#">Rent lorem bla bla Orlando</a></li>
-					<li><a href="#">Rent lorem bla bla Orlando</a></li>
-				</ul>
-			</aside>
-			<aside class="col-lg-3">
-				<h4>KEY WORDS </h4>
-				<ul class="list-no-bullets">
-					<li><a href="#">Rent lorem bla bla Miami</a></li>
-					<li><a href="#">Rent lorem bla bla Miami</a></li>
-					<li><a href="#">Rent lorem bla bla Miami</a></li>
-					<li><a href="#">Rent lorem bla bla Miami</a></li>
-					<li><a href="#">Rent lorem bla bla Miami</a></li>
-				</ul>
-			</aside>
-			<aside class="col-lg-3">
-				<h4>KEY WORDS </h4>
-				<ul class="list-no-bullets">
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-				</ul>
-			</aside>
-			<aside class="col-lg-3">
-				<h4>KEY WORDS </h4>
-				<ul class="list-no-bullets">
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-					<li><a href="#">Rent lorem bla bla Neywork</a></li>
-				</ul>
-			</aside>
-		</div>
-		<div class="row">
-			<hr>
-			<div class="col-lg-6 padding-1em">
-				<p>&copy; 2013 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
-			</div>
-			<div class="col-lg-6  padding-1em">
-				<p class="pull-right"><a href="http://mondedesign.net" target="_blank">By : Monde Design</a></p>
-			</div>
-		</div>
-	</div>
-</footer>
+    <!--common script for all pages-->
+    <script src="/Assets/dashboard/js/common-scripts.js"></script>
 
+    <!--script for this page-->
+    <script src="/Assets/dashboard/js/sparkline-chart.js"></script>
+    <script src="/Assets/dashboard/js/easy-pie-chart.js"></script>
 
+  <script>
 
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+      //owl carousel
 
-<script>window.jQuery || document.write('<script src="/Assets/js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
-<script src="/Assets/js/min/bootstrap.min.js"></script>
-<!-- <script src="/Assets/js/min/typeahead.min.js"></script>
--->
-<script src="/Assets/js/min/bootstrap-select.min.js"></script>
-<script type="text/javascript" src="/Assets/js/bootstrap-datetimepicker.min.js" charset="UTF-8"></script>
-<script type="text/javascript" src="/Assets/js/jquery.eislideshow.js"></script>
-<script type="text/javascript" src="/Assets/js/jquery.easing.1.3.js"></script>
-<!-- price range -->
-<script type="text/javascript" src="/Assets/js/bootstrap-slider.js"></script>
-<script src="/Assets/js/main.js"></script>
-<script src="/Assets/js/app/login.js"></script>
+      $(document).ready(function() {
+          $("#owl-demo").owlCarousel({
+              navigation : true,
+              slideSpeed : 300,
+              paginationSpeed : 400,
+              singleItem : true
 
-<script src="/Assets/js/vendor/modernizr-2.6.2.min.js"></script>
-<?php
-if(defined('JS_PAGE')) {
-	if(is_array(JS_PAGE))
-	{
-		foreach(JS_PAGE as $js)
-		{
-		?>
-			<script type="text/javascript" src="<?=$js?>"></script>
-		<?php
-		}
-	}
-	else
-	{?>
-		<script type="text/javascript" src="<?=JS_PAGE?>"></script>
-<?php
-	}
-}?>
+          });
+      });
 
-<!--
-<? if ($condition): ?>
-  <p>Content</p>
-<? elseif ($other_condition): ?>
-  <p>Other Content</p>
-<? else: ?>
-  <p>Default Content</p>
-<? endif; ?>
--->
-</body>
+      //custom select box
+
+      $(function(){
+          $('select.styled').customSelect();
+      });
+
+  </script>
+
+  </body>
 </html>
