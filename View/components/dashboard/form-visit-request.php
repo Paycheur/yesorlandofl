732b1ddb2501526783a974c14fe19b2ef0d8bde9
Yoann <ylefevre@zenithys.com>
2013-12-23 20:18:33 +0100

    no message

---
+++ View/components/dashboard/form-visit-request.php
@@ -0,0 +1,50 @@
+<div id="modalVisitRequest" class="modal fade"  role="dialog" >
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h3 id="myModalLabel">EDIT A DATE AND TIME</h3>
+			</div>
+			<div class="modal-body padding-bottom-3em">
+				<form action="" id="formVisitRequest" class="form-horizontal" role="form">
+					 <div class="form-group">
+                                      <label class="col-lg-2 control-label">Date : <i class="icon-calendar-empty"></i></label>
+                                      <div class="col-lg-6">
+                                          <input type="text" " name="visit_date" class="form-control"  id="dataPickerInput" readonly >
+                                      </div>
+                                  </div>
+                                  <div class="form-group">
+                                      <label class="col-lg-2 control-label">Hour : <i class="icon-time"></i></label>
+                                      <div class="col-lg-6">
+                                          <input type="time" name="visit_hour" class="form-control">
+                                      </div>
+                                  </div>
+					
+					<div class="form-group">
+                                      <div class="col-lg-offset-2 col-lg-10">
+                                          <button id="submit" class="btn btn-primary">Submit</button>
+                                      </div>
+                                  </div>
+				</form>
+				<hr>
+				<div class="row padding-1em">
+					<div class="col-lg-2">
+						<img src="/Assets/img/jere.jpg" class="img-circle">
+					</div>
+					<span class="col-lg-6" style="margin-left:20px">
+					<hgroup>
+					<h5 class="margin-zero lh-100">JERE MATHENY</h5>
+					<h6 class="margin-zero lh-100"><small>will send a confirmation under 12h</small></h6>
+					</hgroup>
+					<p>
+					<i class="icon-mobile-phone"></i> 407-697-2176
+					</p>
+					</span>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+			</div>
+		</div>
+	</div>
+	</div><!-- model -->
